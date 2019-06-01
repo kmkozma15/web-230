ade8f10144446589498de8f8aafe660fc7687a45
Keith Kozma <kmkozma15@msn.com>
2019-05-31 22:27:14 -0700

    Adding Assignment 1.4

---
+++ Assignment 1.4.js
@@ -0,0 +1,33 @@
+ /* 
+Title:Exercise 1.3
+Author: Keith Kozma
+Date: May 31, 2019
+Description: This program will display a message which says, "Hello World, my name is Keith Kozma. 
+*/
+
+// Variable Declaration
+
+var firstName;
+var lastName;
+var age;
+var street;
+var city;
+var zipCode;
+
+// Variable Assignments
+
+firstName = "Keith";
+lastName = "Kozma";
+age = "25";
+street = "Kirkland dr.";
+city = "Sacramento";
+zipCode = "12345";
+
+// Console Declaration
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(zipCode);
