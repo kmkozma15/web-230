d3c6d891bee17c8303ddba0b0af59948dfb1a2fb
Keith Kozma <kmkozma15@msn.com>
2019-05-31 22:00:50 -0700

    Added in comments to program

--- week-1/hello-world.js
+++ week-1/hello-world.js
@@ -1,1 +1,8 @@
+ /* 
+Title:Exercise 1.3
+Author: Keith Kozma
+Date: May 31, 2019
+Description: This program will display a message which says, "Hello World, my name is Keith Kozma". 
+*/
+
 console.log("Hello World, my name is Keith Kozma")
