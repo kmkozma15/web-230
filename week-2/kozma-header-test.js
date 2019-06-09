52809f3de106f45d5082b4f589265f60f05556a0
Keith Kozma <kmkozma15@msn.com>
2019-06-08 22:51:19 -0700

    Adding .editorconfig, .gitignore, and header files

---
+++ week-2/kozma-header-test.js
@@ -0,0 +1,12 @@
+/*
+============================================
+; Title:  kozma-header-test.js
+; Author: Keith Kozma
+; Date:   8 June 2019
+; Description: Displays a formatted header
+;===========================================
+*/
+
+const header = require('../kozma-header.js');
+
+console.log(header.display("Keith", "Kozma", "Week 2"));
