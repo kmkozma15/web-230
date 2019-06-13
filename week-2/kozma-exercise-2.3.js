d2e303ff6a80705f53de74341ba3c97335634668
Keith Kozma <kmkozma15@msn.com>
2019-06-13 16:11:00 -0700

    Uploading Assignment 2.3

---
+++ week-2/kozma-exercise-2.3.js
@@ -0,0 +1,37 @@
+/*
+============================================
+; Title:  kozma-exercise-2.3.js
+; Author: Keith Kozma
+; Date:   8 June 2019
+; Description: This program
+;===========================================
+*/
+
+/*
+  Expected output: "Hello Keith Kozma!""
+  FirstName LastName
+  Exercise 2.3
+  Today's Date
+  Hello FirstName LastName!
+  Hint: Use your personal header display function and
+  refer to page 178 for implementing function properties
+*/
+
+// Header file output
+var header = require("../kozma-header.js")
+console.log(header.display("Keith" , "Kozma", "Week 2") + '\n')
+
+// Function properties
+myName.keith = "Keith";
+lastName.kozma = "Kozma";
+
+// Main function
+function myName() {
+  return myName.keith;
+}
+function lastName() {
+  return lastName.kozma;
+}
+
+// console.log output
+console.log("Hello " + myName() + " " + lastName() + "!");
