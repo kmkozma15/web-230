5ddb69dcde9f32ae77fa0a019ee9b771f815058f
Keith Kozma <kmkozma15@msn.com>
2019-06-28 21:38:45 -0700

    Uploading Discussion 5.1

---
+++ week-5/discussion-5.1/discussion-5.1.js
@@ -0,0 +1,20 @@
+// Header file output
+var header = require("../kozma-header.js")
+console.log(header.display("Keith" , "Kozma", "Discussion 5.1") + '\n');
+/*
+    Title:Discussion 5.1
+    Author: https://www.w3resource.com
+    Modified by: Keith Kozma (Include errors)
+    Date: June 20, 2019
+    Description: Using a modified exercise from w3resources, this file contains multiple errors.
+*/
+var favFoods = new Map();
+favFoods.set('spaghetti', 'meatballs');
+favFoods.set('beans', 'rice');
+favFoods.set('fruit', 'salad');
+favFoods.set('water', 'lemon');
+favFoods.set('biscuits', 'gravy');
+
+for (var [key, value] of favFoods) {
+  console.log(key + " with " + value);
+}
